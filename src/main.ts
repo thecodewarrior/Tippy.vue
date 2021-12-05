5b1d39bb069b0e44ff00127cb1f8f5bd04fa1c41
Pierce Corcoran <pierce.corcoran@dfstudio.com>
2021-12-05 09:52:10 -0800

    Add install option to set global tippy defaults

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import {Plugin} from "vue";
 import Tippy from "./Tippy";
 import TippyDirective from "./TippyDirective";
 import TippySingleton from "./TippySingleton";
+import tippy, {DefaultProps} from "tippy.js";
 
 const TippyVue: Plugin = {
-  install(app, ...options: any[]) {
+  install(app, defaultProps?: Partial<DefaultProps>) {
+    if(defaultProps) {
+      tippy.setDefaultProps(defaultProps)
+    }
     app.directive('tippy', TippyDirective)
     app.component('tippy', Tippy)
     app.component('tippy-singleton', TippySingleton)
